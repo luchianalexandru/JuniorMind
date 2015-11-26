b2c74df28e5d05162d7b5efd586f1ee35c50170c
Luchian Alex <luchianalex@yahoo.com>
2015-11-26 23:56:22 +0200

    Calculate the number of wood tiles needed for test 2

--- JuniorMind/JuniorMind/Parchet/Parchet.cs
+++ JuniorMind/JuniorMind/Parchet/Parchet.cs
@@ -13,6 +13,14 @@ namespace Parchet
             Assert.AreEqual(15.333333333333333333333333333m, requiredTylesNr);
         }
 
+        [TestMethod]
+        public void NecessaryWoodTiles2()
+        {
+            decimal requiredTylesNr = CalculateWoodTilesNeeded(15, 12, 3, 0.5m);
+            Assert.AreEqual(138, requiredTylesNr);
+        }
+
+
         decimal CalculateWoodTilesNeeded(decimal roomLength, decimal roomWidth, decimal woodTileLength, decimal woodTilewidth)
         {
             return ( roomLength * roomWidth * 1.15m) / ( woodTileLength * woodTilewidth);
