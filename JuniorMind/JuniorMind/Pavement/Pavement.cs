835b3b6e0e0388b4470e74dc60598c1fc8c4c5e3
Luchian Alex <luchianalex@yahoo.com>
2015-11-29 17:42:37 +0200

    Run second test with tile length and width still equal

--- JuniorMind/JuniorMind/Pavement/Pavement.cs
+++ JuniorMind/JuniorMind/Pavement/Pavement.cs
@@ -12,6 +12,11 @@ namespace Pavement
             Assert.AreEqual(12, requiredPavementTiles(10, 7, 3, 3));
         }
 
+        [TestMethod]
+        public void TestForSecondCase()
+        {
+            Assert.AreEqual(2, requiredPavementTiles(6, 3, 3, 3));
+        }
 
         int requiredPavementTiles(int marketLength, int marketWidth, int tileLength, int tileWidth)
         {
@@ -32,5 +37,7 @@ namespace Pavement
             return requiredTilesForMarketLength * requiredTilesForMarketWidth;
 
         }
+
+
     }
 }
