cbe35bcd834747fb89c3739c264fc5a26ea8a4a1
Luchian Alex <luchianalex@yahoo.com>
2015-11-29 17:31:43 +0200

    Calculate solution and run first test

--- JuniorMind/JuniorMind/Pavement/Pavement.cs
+++ JuniorMind/JuniorMind/Pavement/Pavement.cs
@@ -4,11 +4,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Pavement
 {
     [TestClass]
-    public class UnitTest1
+    public class Pavement
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForFirstCase()
         {
+            Assert.AreEqual(12, requiredPavementTiles(10, 7, 3, 3));
+        }
+
+
+        int requiredPavementTiles(int marketLength, int marketWidth, int tileLength, int tileWidth)
+        {
+
+            int requiredTilesForMarketLength = marketLength / tileLength;
+            int requiredTilesForMarketWidth = marketWidth / tileWidth; 
+
+            if (marketLength - (requiredTilesForMarketLength*tileLength) > 0)
+            {
+                requiredTilesForMarketLength++;
+            }
+
+            if (marketWidth - ( requiredTilesForMarketWidth * tileWidth) > 0)
+            {
+                requiredTilesForMarketWidth++;
+            }
+
+            return requiredTilesForMarketLength * requiredTilesForMarketWidth;
+
         }
     }
 }
