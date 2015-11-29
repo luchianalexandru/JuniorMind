c7327da68e2559643826427bc51e5508484e6201
Luchian Alex <luchianalex@yahoo.com>
2015-11-29 19:21:03 +0200

    Calculate Solution and run first test

--- JuniorMind/JuniorMind/ArchaeologyArea/ArchaeologyArea.cs
+++ JuniorMind/JuniorMind/ArchaeologyArea/ArchaeologyArea.cs
@@ -4,11 +4,22 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ArchaeologyArea
 {
     [TestClass]
-    public class UnitTest1
+    public class ArchaeologyArea
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Firsttest()
         {
+            Assert.AreEqual(7.1m, HouseArea(5, 3.1m, 6, 2.1m, 7.2m, 8));
+
         }
+
+        decimal HouseArea(decimal x1, decimal y1, decimal x2, decimal y2, decimal x3, decimal y3)
+        {
+
+            decimal AreaDeterminant = ((x1 * y2 * 1) + (x2 * y3 * 1) + (x3 * y1 * 1) - (x3 * y2 * 1) - (x1 * y3 * 1) - (x2 * y1 * 1));
+            decimal AbsoluteValueOfDeterminant = Math.Abs(AreaDeterminant);
+            return 2 * 0.5m * AbsoluteValueOfDeterminant;
+        }
+
     }
 }
