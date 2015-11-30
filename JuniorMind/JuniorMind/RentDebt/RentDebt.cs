ce64770445f7bcf12a2c4fb2d5ff133aa6c4944a
Luchian Alex <luchianalex@yahoo.com>
2015-12-01 00:03:07 +0200

    First test for first month overdue

--- JuniorMind/JuniorMind/RentDebt/RentDebt.cs
+++ JuniorMind/JuniorMind/RentDebt/RentDebt.cs
@@ -4,11 +4,32 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace RentDebt
 {
     [TestClass]
-    public class UnitTest1
+    public class RentDebt
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FirstTest()
         {
+            Assert.AreEqual(102, calculateDebt(100, 1));
+        }
+
+        decimal calculateDebt (int rent, int nrOfDaysLate)
+        {
+            int percentIfLate = 0;
+            if (nrOfDaysLate <= 10)
+            {
+             percentIfLate = 2;
+            }
+            else if (nrOfDaysLate <= 30)
+            {
+             percentIfLate = 5;
+            }
+            else if (nrOfDaysLate <= 40)
+            {
+             percentIfLate = 10;
+            }
+            decimal penalty = ( rent * percentIfLate) / 100;
+            return rent + penalty * nrOfDaysLate;
+
         }
     }
 }
