76618681d1cfb4eaf0b520c3610356627503a782
Luchian Alex <luchianalex@yahoo.com>
2015-12-01 18:08:33 +0200

    Calculate solution and run first test, for weight under 3kg.

--- JuniorMind/JuniorMind/Melons/Melons.cs
+++ JuniorMind/JuniorMind/Melons/Melons.cs
@@ -7,8 +7,22 @@ namespace Melons
     public class Melons
     {
         [TestMethod]
-        public void TestMethod1()
+        public void testfor3Kg()
         {
+            Assert.AreEqual("The melon can't be divided by even numbers!", melon(3));
+        }
+
+        string melon(int weight)
+        {
+            if (weight < 4)
+            {
+                return "The melon can't be divided by even numbers!";
+            }
+            if (weight % 2 == 0)
+            {
+                return "DA!";
+            }
+            else return "NU!";
         }
     }
 }
