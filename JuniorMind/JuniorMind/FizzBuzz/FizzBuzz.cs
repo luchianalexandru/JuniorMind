a818cf7f5eccdabc2a7f5c7bb8de86949dd84872
Luchian Alex <luchianalex@yahoo.com>
2015-12-02 23:16:49 +0200

    Create solution and run test of a random number which can not be divided by either 3 or 5.

--- JuniorMind/JuniorMind/FizzBuzz/FizzBuzz.cs
+++ JuniorMind/JuniorMind/FizzBuzz/FizzBuzz.cs
@@ -7,8 +7,27 @@ namespace FizzBuzz
     public class FizzBuzz
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Fizzbuzz1()
         {
+            Assert.AreEqual("Numarul nu este divizibil cu 3 sau 5", FizzSauBuzz(17));
+        }
+
+        string FizzSauBuzz(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz!";
+            }
+            else if (number % 3 == 0)
+            {
+                return "Fizz!";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Buzz!";
+            }
+            else return "Numarul nu este divizibil cu 3 sau 5";
         }
     }
 }
+
