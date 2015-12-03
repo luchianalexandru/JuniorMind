d6e1dc6ab86315a686dc0746ae1295afdcbfc114
Luchian Alex <luchianalex@yahoo.com>
2015-12-03 21:37:01 +0200

    Add first test for short distance in the morning.

--- JuniorMind/JuniorMind/TaxiPrice/TaxiPrice.cs
+++ JuniorMind/JuniorMind/TaxiPrice/TaxiPrice.cs
@@ -7,8 +7,49 @@ namespace TaxiPrice
     public class TaxiPrice
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForDayShortDistance()
         {
+            Assert.AreEqual(5, TaxiPriceCalculation(1, 9));
+        }
+
+
+
+        int TaxiPriceCalculation(int distanceInKm, int hour)
+        {
+            int pricePerKm = 0;
+            if (hour > 8 && hour < 21)
+            {
+                if (distanceInKm >= 1 && distanceInKm <= 20)
+                {
+                    pricePerKm = 5;
+                }
+                else if (distanceInKm >= 21 && distanceInKm <= 60)
+                {
+                    pricePerKm = 8;
+                }
+                else if (distanceInKm >= 60)
+                {
+                    pricePerKm = 6;
+                }
+            }
+            else if (hour >= 21)
+            {
+
+                if (distanceInKm >= 1 && distanceInKm <= 20)
+                {
+                    pricePerKm = 7;
+                }
+                else if (distanceInKm >= 21 && distanceInKm <= 60)
+                {
+                    pricePerKm = 10;
+                }
+                else if (distanceInKm >= 60)
+                {
+                    pricePerKm = 8;
+                }
+            }
+            return distanceInKm * pricePerKm;
         }
     }
 }
+
