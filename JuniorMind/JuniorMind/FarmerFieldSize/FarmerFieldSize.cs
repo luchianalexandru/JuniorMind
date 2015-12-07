db29f886f4b1e5c9d6ce5f93aecba345f12ccab4
Luchian Alex <luchianalex@yahoo.com>
2015-12-07 21:00:31 +0200

    Add empty FarmerFieldSize project.

---
+++ JuniorMind/JuniorMind/FarmerFieldSize/FarmerFieldSize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FarmerFieldSize
+{
+    [TestClass]
+    public class FarmerFieldSize
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
