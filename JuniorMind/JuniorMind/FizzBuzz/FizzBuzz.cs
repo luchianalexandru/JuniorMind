90f534f0533273de2bcf70348b6c6dfa905e70e9
Luchian Alex <luchianalex@yahoo.com>
2015-12-07 21:47:13 +0200

    Removed commented lines from FizzBuzz TDD.

--- JuniorMind/JuniorMind/FizzBuzz/FizzBuzz.cs
+++ JuniorMind/JuniorMind/FizzBuzz/FizzBuzz.cs
@@ -33,19 +33,6 @@ namespace FizzBuzz
         string FizzSauBuzz(int number)
         {
             return number % 3 == 0 && number % 5 == 0 ? "FizzBuzz!" : number % 3 == 0 ? "Fizz!" : number % 5 == 0 ? "Buzz!" : "Numarul nu este divizibil cu 3 sau 5";
-            //  if (number % 3 == 0 && number % 5 == 0)
-            //  {
-            //      return "FizzBuzz!";
-            // }
-            //    else if (number % 3 == 0)
-            //   {
-            //        return "Fizz!";
-            //  }
-            //   else if (number % 5 == 0)
-            //   {
-            //   return "Buzz!";
-            // }
-            //    else return "Numarul nu este divizibil cu 3 sau 5";
         }
     }
 }
