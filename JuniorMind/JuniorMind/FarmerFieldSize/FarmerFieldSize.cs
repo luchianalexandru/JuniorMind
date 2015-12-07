a723ae11e54f8c2358835ebbb0ae01521b2ae033
Luchian Alex <luchianalex@yahoo.com>
2015-12-07 21:23:28 +0200

    FieldSizeTest1 works.

--- JuniorMind/JuniorMind/FarmerFieldSize/FarmerFieldSize.cs
+++ JuniorMind/JuniorMind/FarmerFieldSize/FarmerFieldSize.cs
@@ -14,7 +14,24 @@ namespace FarmerFieldSize
 
         double fieldSizeCalculation(int additionalLength, int totalArea)
         {
-        return 5;
+            //functia de gradul 2: a*x*x +b*x - c = 0 
+            //discriminantul functiei de gradul 2: Delta= b*b + 4*a*c
+            //a = 1;
+            //b = additionalLength
+            //c = totalArea
+
+            double Delta = additionalLength * additionalLength + 4 * totalArea;
+
+            //solutia ecuatiei de gradul doi = s(x1, x2)
+            // x1= (-b+ sqrt(Delta))/2a; x2 = (-b-sqrt(Delta))/2a;
+            double x1 = (-additionalLength + Math.Sqrt(Delta)) / 2;
+            double x2 = (-additionalLength + Math.Sqrt(Delta)) / 2;
+
+            if (x1 > 0)
+            {
+                return x1;
+            }
+            else return x2;
         }
     }
 }
