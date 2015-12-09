c43722f9713281979c487492d231fcadaf5974ec
Luchian Alex <luchianalex@yahoo.com>
2015-12-09 19:15:21 +0200

    Add empty ArabToRoman Project

---
+++ JuniorMind/JuniorMind/ArabToRoman/ArabToRoman.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ArabToRoman
+{
+    [TestClass]
+    public class ArabToRoman
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    
+  
+    }
+
+    //Convertește un număr dat, între 1 și 100, în cifre romane.
+
+    //I - 1
+    //IV - 4
+    //V - 5
+    //IX - 9
+    //X - 10
+    //L - 50
+    //XC - 90
+    //C - 100
+}
