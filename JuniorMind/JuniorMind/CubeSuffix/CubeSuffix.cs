f5efe90804bae6fd65d9657e3ace69ff3811ee81
Luchian Alex <luchianalex@yahoo.com>
2015-12-14 21:36:20 +0200

    CubeSuffix: added empty project.

---
+++ JuniorMind/JuniorMind/CubeSuffix/CubeSuffix.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CubeSuffix
+{
+    [TestClass]
+    public class CubeSuffix
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
