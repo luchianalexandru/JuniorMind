bf7aa8cb6a5689a3400f6bbe1c969fb56bb06cd4
Luchian Alex <luchianalex@yahoo.com>
2015-12-14 21:22:54 +0200

    WordPrefix: added more tests.

--- JuniorMind/JuniorMind/WordsPrefix/WordsPrefix.cs
+++ JuniorMind/JuniorMind/WordsPrefix/WordsPrefix.cs
@@ -6,11 +6,11 @@ namespace WordsPrefix
     [TestClass]
     public class WordsPrefix
     {
-        //[TestMethod]
-        //public void FirstTest()
-        //{
-        //    Assert.AreEqual("a", wordsPrefix("a", "ab"));
-        //}
+        [TestMethod]
+        public void FirstTest()
+        {
+            Assert.AreEqual("a", wordsPrefix("a", "ab"));
+        }
 
         [TestMethod]
         public void SecondTestForNoPrefix()
@@ -18,9 +18,29 @@ namespace WordsPrefix
             Assert.AreEqual("", wordsPrefix("a", "b"));
         }
 
+        [TestMethod]
+        public void ThirdTestForABCDE()
+        {
+            Assert.AreEqual("abc", wordsPrefix("abc", "abcde"));
+        }
+
+        [TestMethod]
+        public void FourthTestForSeaShells()
+        {
+            Assert.AreEqual("seashells", wordsPrefix("seashells", "seashellsontheseashore"));
+        }
+
+
+
         string wordsPrefix ( string firstWord, string secondWord)
         {
             string prefix = "";
+            int shortestWord = Math.Min(firstWord.Length, secondWord.Length);
+            for ( int i = 0; i < shortestWord; i++)
+            {
+                if (firstWord[i] == secondWord[i]) prefix += firstWord[i];
+                else break;
+            }
             return prefix;
         }
     }
