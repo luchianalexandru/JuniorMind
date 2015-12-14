0abbb945ac497469ebbdfb9ff9ea60289592a4e4
Luchian Alex <luchianalex@yahoo.com>
2015-12-14 20:21:05 +0200

    Add empty Chessboard Project.

---
+++ JuniorMind/JuniorMind/ChessBoard/ChessBoard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessBoard
+{
+    [TestClass]
+    public class ChessBoard
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
