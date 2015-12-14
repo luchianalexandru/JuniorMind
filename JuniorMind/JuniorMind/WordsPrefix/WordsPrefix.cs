54debc6268d1e536edcf19bdf0d569ba5c71de89
Luchian Alex <luchianalex@yahoo.com>
2015-12-14 21:00:31 +0200

    Added empty WordsPrefix Project.

---
+++ JuniorMind/JuniorMind/WordsPrefix/WordsPrefix.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordsPrefix
+{
+    [TestClass]
+    public class WordsPrefix
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
