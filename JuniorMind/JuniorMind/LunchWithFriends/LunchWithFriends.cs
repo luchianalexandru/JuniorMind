842057132e82c1a8eb4e2c7d0c605ab4c7c15240
luchianalexandru <luchianalex@yahoo.com>
2015-12-16 20:05:47 +0200

    LunchWithFriends: added some more tests and modified the initiation for i.

--- JuniorMind/JuniorMind/LunchWithFriends/LunchWithFriends.cs
+++ JuniorMind/JuniorMind/LunchWithFriends/LunchWithFriends.cs
@@ -9,25 +9,37 @@ namespace LunchWithFriends
         [TestMethod]
         public void TestFor4And6()
         {
-            Assert.AreEqual(12, CalculateBiggestCommonMultiplier(4, 6));
+            Assert.AreEqual(12, CalculateLeastCommonMultiplier(4, 6));
         }
 
         [TestMethod]
         public void TestFor7And11()
         {
-            Assert.AreEqual(77, CalculateBiggestCommonMultiplier(7, 11));
+            Assert.AreEqual(77, CalculateLeastCommonMultiplier(7, 11));
         }
 
         [TestMethod]
         public void TestFor9And12()
         {
-            Assert.AreEqual(36, CalculateBiggestCommonMultiplier(9, 12));
+            Assert.AreEqual(36, CalculateLeastCommonMultiplier(9, 12));
         }
 
-        public int CalculateBiggestCommonMultiplier(int nr1, int nr2)
+        [TestMethod]
+        public void TestFor12And32()
         {
+            Assert.AreEqual(96, CalculateLeastCommonMultiplier(12, 32));
+        }
 
-            for(int i=1; ;i++)
+        [TestMethod]
+        public void TestFor73And54()
+        {
+            Assert.AreEqual(3942, CalculateLeastCommonMultiplier(73, 54));
+        }
+
+        public int CalculateLeastCommonMultiplier(int nr1, int nr2)
+        {
+            int max = Math.Max(nr1, nr2);
+            for(int i= max + 1; ;i++)
             {
                 if ( i%nr1 == 0 && i%nr2 == 0)
                 {
