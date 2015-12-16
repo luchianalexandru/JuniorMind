41140f5168b3ab30269138f30ff6a17e4c05322b
luchianalexandru <luchianalex@yahoo.com>
2015-12-16 19:56:26 +0200

    LunchWithFriends: added first test.

--- JuniorMind/JuniorMind/LunchWithFriends/LunchWithFriends.cs
+++ JuniorMind/JuniorMind/LunchWithFriends/LunchWithFriends.cs
@@ -7,9 +7,16 @@ namespace LunchWithFriends
     public class LunchWithFriends
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestFor4And6()
         {
+            Assert.AreEqual(0, CalculateBiggestCommonMultiplier(4, 6));
         }
 
+
+        public int CalculateBiggestCommonMultiplier(int nr1, int nr2)
+        {
+            return 0;
+
+        }
     }
 }
