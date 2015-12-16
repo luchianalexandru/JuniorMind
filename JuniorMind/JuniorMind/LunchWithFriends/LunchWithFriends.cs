37f6cbd796a9081da6134210139cecd3a0504470
luchianalexandru <luchianalex@yahoo.com>
2015-12-16 20:00:33 +0200

    LunchWithFriends: added several tests

--- JuniorMind/JuniorMind/LunchWithFriends/LunchWithFriends.cs
+++ JuniorMind/JuniorMind/LunchWithFriends/LunchWithFriends.cs
@@ -9,13 +9,32 @@ namespace LunchWithFriends
         [TestMethod]
         public void TestFor4And6()
         {
-            Assert.AreEqual(0, CalculateBiggestCommonMultiplier(4, 6));
+            Assert.AreEqual(12, CalculateBiggestCommonMultiplier(4, 6));
         }
 
+        [TestMethod]
+        public void TestFor7And11()
+        {
+            Assert.AreEqual(77, CalculateBiggestCommonMultiplier(7, 11));
+        }
+
+        [TestMethod]
+        public void TestFor9And12()
+        {
+            Assert.AreEqual(36, CalculateBiggestCommonMultiplier(9, 12));
+        }
 
         public int CalculateBiggestCommonMultiplier(int nr1, int nr2)
         {
-            return 0;
+
+            for(int i=1; ;i++)
+            {
+                if ( i%nr1 == 0 && i%nr2 == 0)
+                {
+                    return i;
+                }
+
+            }
 
         }
     }
