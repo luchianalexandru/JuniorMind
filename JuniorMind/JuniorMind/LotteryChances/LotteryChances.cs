91af474d5a9fcc418124da41c5ff8a0426d53bec
luchianalexandru <luchianalex@yahoo.com>
2015-12-16 21:25:56 +0200

    LotteryChances: add empty project.

---
+++ JuniorMind/JuniorMind/LotteryChances/LotteryChances.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LotteryChances
+{
+    [TestClass]
+    public class LotteryChances
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
