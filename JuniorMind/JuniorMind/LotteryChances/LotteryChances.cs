bc8646e6073e762305180c12e3a0f5e132343c77
luchianalexandru <luchianalex@yahoo.com>
2015-12-16 22:27:18 +0200

    LotteryChances: added some tests for 1 chance out of X.

--- JuniorMind/JuniorMind/LotteryChances/LotteryChances.cs
+++ JuniorMind/JuniorMind/LotteryChances/LotteryChances.cs
@@ -7,9 +7,35 @@ namespace LotteryChances
     public class LotteryChances
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LottoChance1of1()
         {
+            Assert.AreEqual(1, CalculateLotteryOdds(1, 1));
         }
 
+        [TestMethod]
+        public void LottoChance1of2()
+        {
+            Assert.AreEqual(0.5, CalculateLotteryOdds(1, 2));
+        }
+
+        [TestMethod]
+        public void LottoChance1of3()
+        {
+            Assert.AreEqual(0.3333333d, CalculateLotteryOdds(1, 3),0.0000001);
+        }
+
+        [TestMethod]
+        public void LottoChance1of4()
+        {
+            Assert.AreEqual(0.25d, CalculateLotteryOdds(1, 4), 0.01);
+        }
+
+        double CalculateLotteryOdds(double NrOfPicks, double RangeOfNrToPick)
+        {
+            double chance;
+            chance = NrOfPicks / RangeOfNrToPick;
+            return chance;
+
+        }
     }
 }
