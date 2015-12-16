73bf4d3449f960aaaa113c48d953955912823455
luchianalexandru <luchianalex@yahoo.com>
2015-12-16 19:54:09 +0200

    Add empty LunchWithFriends Project

---
+++ JuniorMind/JuniorMind/LunchWithFriends/LunchWithFriends.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LunchWithFriends
+{
+    [TestClass]
+    public class LunchWithFriends
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
