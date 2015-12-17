7074ef6faae5eaada38c0909263390147e9d5563
Luchian Alex <luchianalex@yahoo.com>
2015-12-17 21:20:05 +0200

    Pangram: tests fail if sentence longer than 26.

--- JuniorMind/JuniorMind/Pangram/Pangram.cs
+++ JuniorMind/JuniorMind/Pangram/Pangram.cs
@@ -12,10 +12,41 @@ namespace Pangram
             Assert.AreEqual(true, IsPangram("abcdefghijklmnopqrstuvwxyz"));
         }
 
+        [TestMethod]
+        public void TestTwoSentenceIsShorterThan26()
+        {
+            Assert.AreEqual(false, IsPangram("abcdefghijklmnopqrstuvwxy"));
+        }
+        
+       [TestMethod]
+        public void TestThreeDifferentPangramLongerThan26()
+        {
+            Assert.AreEqual(true, IsPangram("thequickbrownfoxjumpsoverthelazydog"));
+        }
+
+        [TestMethod]
+        public void TestFourSentenceIsExactly26()
+        {
+            Assert.AreEqual(true, IsPangram("mrjocktvquizphdbagsfewlynx"));
+        }
+
+
         bool IsPangram(string sentence)
         {
             string[] pangramContents = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-            return SentenceLength(sentence);
+            if (!SentenceLength(sentence))
+            {
+                return false;
+            }
+            for(int i=0;i<sentence.Length ;i++)
+            {
+                if (!sentence.Contains(pangramContents[i]))
+                {
+                    return false;
+
+                }
+            }
+            return true;
         }
 
         private static bool SentenceLength(string sentence)
