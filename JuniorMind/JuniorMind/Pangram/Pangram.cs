1f6186a418a609c91c21d29598effa4a129ef2b4
Luchian Alex <luchianalex@yahoo.com>
2015-12-17 20:46:45 +0200

    Add empty Pangram Project

---
+++ JuniorMind/JuniorMind/Pangram/Pangram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pangram
+{
+    [TestClass]
+    public class Pangram
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
