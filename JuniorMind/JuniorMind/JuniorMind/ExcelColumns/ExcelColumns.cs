59d9f4fc3d5825f319f26524e8307e2b5d115b69
luchianalexandru <luchianalex@yahoo.com>
2015-12-23 18:23:10 +0200

    ExcelColumns: add new empty project

---
+++ JuniorMind/JuniorMind/JuniorMind/ExcelColumns/ExcelColumns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExcelColumns
+{
+    [TestClass]
+    public class ExcelColumns
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+
+
+    }
+
+}
