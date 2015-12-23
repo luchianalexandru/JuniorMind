ad007dd3ac2eddc48549d1bcf06c7fc2ee77c384
luchianalexandru <luchianalex@yahoo.com>
2015-12-23 20:16:11 +0200

    ExcelColumns: add a test for when the number is up to 26.

--- JuniorMind/JuniorMind/JuniorMind/ExcelColumns/ExcelColumns.cs
+++ JuniorMind/JuniorMind/JuniorMind/ExcelColumns/ExcelColumns.cs
@@ -9,11 +9,21 @@ namespace ExcelColumns
         [TestMethod]
         public void FirstTestWithNothingInString()
         {
-            Assert.AreEqual("", ColumnsInExcelForGivenNumber(0));
+            Assert.AreEqual("", ColumnsInExcelForGivenNumber(27));
+        }
+
+        [TestMethod]
+        public void TestForNumberUpTo26()
+        {
+            Assert.AreEqual("p", ColumnsInExcelForGivenNumber(15));
         }
 
         string ColumnsInExcelForGivenNumber(int number)
         {
+            string column = "";
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            if (number <= 26) return column += alphabet[number];
 
             return "";
 
