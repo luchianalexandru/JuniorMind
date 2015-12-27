cb194a8f6c2ce4cfea9e7f72c3d5a2e4a9a20c7d
luchianalexandru <luchianalex@yahoo.com>
2015-12-27 14:07:48 +0200

    Anagrams: added if contained check function for each letter in String.

--- JuniorMind/Anagrams/Anagrams.cs
+++ JuniorMind/Anagrams/Anagrams.cs
@@ -7,11 +7,47 @@ namespace Anagrams
     public class Anagrams
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IfWordContainsTest1()
         {
+            Assert.AreEqual(1, Contains("a",'a'));
+        }
+
+        [TestMethod]
+        public void IfWordContainsTest2()
+        {
+            Assert.AreEqual(3, Contains("aaabbbbcbc", 'a'));
+        }
+
+        [TestMethod]
+        public void IfWordContainsTest3()
+        {
+            Assert.AreEqual(5, Contains("aaabbbbcbc", 'b'));
+        }
+
+  
+        int Contains(string ToCheck, char c)
+        {
+            int contained = 0;
+            for (int i = 0; i < ToCheck.Length; i++)
+            {
+                if (ToCheck[i] == c) contained++;
+
+            }
+            return contained;
         }
 
 
+   
 
+        int CountAnagrams(string word)
+        {
+            int counter = 0;
+            string NewString = "";
+
+           
+
+            return 0;
+
+        }
     }
 }
