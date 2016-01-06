d42e3768a672adfe23867d0cce1099cc6f3d6cda
luchianalexandru <luchianalex@yahoo.com>
2016-01-06 18:53:12 +0200

    BaseConverter: added the solution for converting from base 10 to 2. Results are inversed from what they should be.

--- JuniorMind/BaseConverter/BaseConverter.cs
+++ JuniorMind/BaseConverter/BaseConverter.cs
@@ -9,12 +9,31 @@ namespace BaseConverter
         [TestMethod]
         public void ConvertFromBase10ToBase2Test1()
         {
-            CollectionAssert.AreEqual(new byte[] { }, ConvertFromBase10ToBase2(1));
+            CollectionAssert.AreEqual(new byte[] { 0, 1 }, ConvertFromBase10ToBase2(2));
+        }
+
+        [TestMethod]
+        public void ConvertFromBase10ToBase2Test2()
+        {
+            CollectionAssert.AreEqual(new byte[] { 0, 0, 1 }, ConvertFromBase10ToBase2(4));
+        }
+
+        [TestMethod]
+        public void ConvertFromBase10ToBase2Test3()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 0, 1 }, ConvertFromBase10ToBase2(5));
         }
 
         byte[] ConvertFromBase10ToBase2(int number)
         {
             byte[] array = { };
+
+            while (number > 0)
+            {
+                Array.Resize(ref array, array.Length + 1);
+                array[array.Length - 1] = (byte)(number % 2);
+                number /= 2;
+            }
 
             return array;
         }
