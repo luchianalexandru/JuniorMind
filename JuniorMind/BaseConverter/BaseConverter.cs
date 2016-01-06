9656104c3fb9e64de426b808dc797bfa5f362ecc
luchianalexandru <luchianalex@yahoo.com>
2016-01-06 18:41:09 +0200

    BaseConverter: added empty project.

---
+++ JuniorMind/BaseConverter/BaseConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseConverter
+{
+    [TestClass]
+    public class BaseConverter
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        byte[] ConvertFromBase10ToBase2(int number)
+        {
+            return ;
+        }
+    }
+}
