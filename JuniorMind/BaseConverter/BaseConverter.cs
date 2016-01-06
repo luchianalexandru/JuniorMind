baefdbdd3b34466ddef686915ec31c0decfc4cac
luchianalexandru <luchianalex@yahoo.com>
2016-01-06 19:05:31 +0200

    BaseConverter: added an inverter for the array, now the tests work for the array with the right order.

--- JuniorMind/BaseConverter/BaseConverter.cs
+++ JuniorMind/BaseConverter/BaseConverter.cs
@@ -9,19 +9,37 @@ namespace BaseConverter
         [TestMethod]
         public void ConvertFromBase10ToBase2Test1()
         {
-            CollectionAssert.AreEqual(new byte[] { 0, 1 }, ConvertFromBase10ToBase2(2));
+            CollectionAssert.AreEqual(new byte[] { 1, 0 }, ConvertFromBase10ToBase2(2));
         }
 
         [TestMethod]
         public void ConvertFromBase10ToBase2Test2()
         {
-            CollectionAssert.AreEqual(new byte[] { 0, 0, 1 }, ConvertFromBase10ToBase2(4));
+            CollectionAssert.AreEqual(new byte[] { 1, 0, 0 }, ConvertFromBase10ToBase2(4));
         }
 
         [TestMethod]
         public void ConvertFromBase10ToBase2Test3()
         {
             CollectionAssert.AreEqual(new byte[] { 1, 0, 1 }, ConvertFromBase10ToBase2(5));
+        }
+
+        [TestMethod]
+        public void ConvertFromBase10ToBase2Test4()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 0, 0, 1 }, ConvertFromBase10ToBase2(9));
+        }
+
+        [TestMethod]
+        public void ConvertFromBase10ToBase2Test5()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 0, 0, 0, 1 }, ConvertFromBase10ToBase2(17));
+        }
+
+        [TestMethod]
+        public void ConvertFromBase10ToBase2Test6()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 1, 0, 1, 1, 1, 1, 1 }, ConvertFromBase10ToBase2(223));
         }
 
         byte[] ConvertFromBase10ToBase2(int number)
@@ -35,7 +53,10 @@ namespace BaseConverter
                 number /= 2;
             }
 
-            return array;
+            byte[] reversedArray = new byte[array.Length];
+            for (int i = 0; i < array.Length; i++) reversedArray[i] = array[array.Length - 1 - i];
+            
+            return reversedArray;
         }
     }
 }
