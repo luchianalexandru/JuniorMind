06aed60b1bbe4a62e9bf5bcc2cbd5e1aecb63dcd
luchianalexandru <luchianalex@yahoo.com>
2016-01-07 18:46:59 +0200

    BaseConverter: added the NOT operation.

--- JuniorMind/BaseConverter/BaseConverter.cs
+++ JuniorMind/BaseConverter/BaseConverter.cs
@@ -41,24 +41,7 @@ namespace BaseConverter
         {
             CollectionAssert.AreEqual(new byte[] { 1, 1, 0, 1, 1, 1, 1, 1 }, ConvertFromBase10ToBase2(223));
         }
-
-        byte[] ConvertFromBase10ToBase2(int number)
-        {
-            byte[] array = { };
-
-            while (number > 0)
-            {
-                Array.Resize(ref array, array.Length + 1);
-                array[array.Length - 1] = (byte)(number % 2);
-                number /= 2;
-            }
-
-            //byte[] reversedArray = Inverter(array);
-
-            return Inverter(array);
-        }
-
-
+        
         [TestMethod]
         public void InverterTest1()
         {
@@ -71,11 +54,46 @@ namespace BaseConverter
             CollectionAssert.AreEqual(new byte[] { 3, 9, 7 }, Inverter(new byte[] { 7, 9, 3 }));
         }
 
+
+        byte[] ConvertFromBase10ToBase2(int number)
+        {
+            byte[] array = { };
+
+            while (number > 0)
+            {
+                Array.Resize(ref array, array.Length + 1);
+                array[array.Length - 1] = (byte)(number % 2);
+                number /= 2;
+            }
+
+            return Inverter(array);
+        }
+
+
         private static byte[] Inverter(byte[] array)
         {
             byte[] reversedArray = new byte[array.Length];
             for (int i = 0; i < array.Length; i++) reversedArray[i] = array[array.Length - 1 - i];
             return reversedArray;
+        }
+
+        [TestMethod]
+        public void NotOperationTest1()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 0 }, NotOperation(new byte[] { 0, 1}));
+        }
+
+      
+        byte[] NotOperation(byte[] array)
+        {
+            byte[] notArray = new byte[array.Length];
+
+            for(int i = 0; i < notArray.Length -1 ; i++)
+            {
+                if (array[i] == (byte)0) notArray[i] = (byte)1;
+                else notArray[i] = (byte)0;
+            }
+            return notArray;
         }
     }
 }
