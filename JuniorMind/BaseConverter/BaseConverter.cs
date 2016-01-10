f8bd2fde8427eb30dd6964e31f3b48e9eeafcf50
luchianalexandru <luchianalex@yahoo.com>
2016-01-10 17:49:39 +0200

    BaseConverter: arranged the code a bit.

--- JuniorMind/BaseConverter/BaseConverter.cs
+++ JuniorMind/BaseConverter/BaseConverter.cs
@@ -55,103 +55,6 @@ namespace BaseConverter
         }
 
         [TestMethod]
-        public void NotOperationTest1()
-        {
-            CollectionAssert.AreEqual(new byte[] { 1, 0 }, NotOperation(new byte[] { 0, 1 }));
-        }
-
-        [TestMethod]
-        public void NotOperationTest2()
-        {
-            CollectionAssert.AreEqual(new byte[] { 0, 1, 1 }, NotOperation(new byte[] { 1, 0, 0 }));
-        }
-
-        [TestMethod]
-        public void NotOperationTest3()
-        {
-            CollectionAssert.AreEqual(new byte[] { 0, 1, 1, 1, 0 }, NotOperation(new byte[] { 1, 0, 0, 0, 1 }));
-        }
-
-        [TestMethod]
-        public void NotOperationTest4()
-        {
-            CollectionAssert.AreEqual(new byte[] { 0, 0, 1, 0, 0, 0, 0, 0 }, NotOperation(new byte[] { 1, 1, 0, 1, 1, 1, 1, 1 }));
-        }
-
-        byte[] ConvertFromBase10ToBase2(int number)
-        {
-            byte[] array = { };
-
-            while (number > 0)
-            {
-                Array.Resize(ref array, array.Length + 1);
-                array[array.Length - 1] = (byte)(number % 2);
-                number /= 2;
-            }
-
-            return Inverter(array);
-        }
-
-
-        private static byte[] Inverter(byte[] array)
-        {
-            byte[] reversedArray = new byte[array.Length];
-            for (int i = 0; i < array.Length; i++) reversedArray[i] = array[array.Length - 1 - i];
-            return reversedArray;
-        }
-                
-
-        byte[] NotOperation(byte[] array)
-        {
-            byte[] notArray = new byte[array.Length];
-            for(int i = 0; i < notArray.Length; i++) notArray[i] = array[i] == (byte)0 ? (byte)1 : (byte)0;
-            return notArray;
-        }
-
-        [TestMethod]
-        public void AndOperationTest1()
-        {
-            CollectionAssert.AreEqual(new byte[] { 1, 1}, AndOperation(new byte[] { 1, 1 }, new byte[] { 1, 1 }));
-        }
-
-        [TestMethod]
-        public void AndOperationTest2()
-        {
-            CollectionAssert.AreEqual(new byte[] { 1, 0 }, AndOperation(new byte[] { 1, 1 }, new byte[] { 1, 0 }));
-        }
-        
-        [TestMethod]
-        public void AndOperationTest3()
-        {
-            CollectionAssert.AreEqual(new byte[] { 1, 0, 1, 0 }, AndOperation(new byte[] { 1, 1, 1, 0 }, new byte[] { 1, 0, 1, 0 }));
-        }
-
-        [TestMethod]
-        public void AndOperationTest4()
-        {
-            CollectionAssert.AreEqual(new byte[] { 0, 0, 1, 0, 1, 1 }, AndOperation(new byte[] { 1, 1, 1, 1, 1, 0 }, new byte[] { 1, 0, 1, 0 }));
-        }
-
-        [TestMethod]
-        public void AndOperationTest5()
-        {
-            CollectionAssert.AreEqual(new byte[] { 0, 0, 1, 0, 0 }, AndOperation(new byte[] { 1, 1, 1, 1, 0 }, new byte[] { 1, 0, 1 }));
-        }
-
-        byte[] AndOperation(byte[] arrayA, byte[] arrayB)
-        {
-            int l = Math.Max(arrayA.Length, arrayB.Length);
-            byte[] result = new byte[l];
-
-            if (arrayA.Length < arrayB.Length) arrayA = MakeArraysToHaveSameLength(arrayA, arrayB);
-            else if (arrayA.Length > arrayB.Length) arrayB = MakeArraysToHaveSameLength(arrayB, arrayA);
-
-            for (int i = 0; i < l; i++) result[i] = (arrayA[i] == (byte)1 && arrayB[i] == (byte)1 ? (byte)1 : (byte)0);
-            return result; 
-        }
-
-
-        [TestMethod]
         public void TestForMakingArraysHaveSameLength1()
         {
             CollectionAssert.AreEqual(new byte[] { 0, 1, 1 }, MakeArraysToHaveSameLength(new byte[] { 1, 1 }, new byte[] { 1, 1, 0 }));
@@ -176,18 +79,113 @@ namespace BaseConverter
         }
 
 
+        [TestMethod]
+        public void NotOperationTest1()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 0 }, NotOperation(new byte[] { 0, 1 }));
+        }
+
+        [TestMethod]
+        public void NotOperationTest2()
+        {
+            CollectionAssert.AreEqual(new byte[] { 0, 1, 1 }, NotOperation(new byte[] { 1, 0, 0 }));
+        }
+
+        [TestMethod]
+        public void NotOperationTest3()
+        {
+            CollectionAssert.AreEqual(new byte[] { 0, 1, 1, 1, 0 }, NotOperation(new byte[] { 1, 0, 0, 0, 1 }));
+        }
+
+        [TestMethod]
+        public void NotOperationTest4()
+        {
+            CollectionAssert.AreEqual(new byte[] { 0, 0, 1, 0, 0, 0, 0, 0 }, NotOperation(new byte[] { 1, 1, 0, 1, 1, 1, 1, 1 }));
+        }
+
+        [TestMethod]
+        public void AndOperationTest1()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 1 }, AndOperation(new byte[] { 1, 1 }, new byte[] { 1, 1 }));
+        }
+
+        [TestMethod]
+        public void AndOperationTest2()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 0 }, AndOperation(new byte[] { 1, 1 }, new byte[] { 1, 0 }));
+        }
+
+        [TestMethod]
+        public void AndOperationTest3()
+        {
+            CollectionAssert.AreEqual(new byte[] { 1, 0, 1, 0 }, AndOperation(new byte[] { 1, 1, 1, 0 }, new byte[] { 1, 0, 1, 0 }));
+        }
+
+        [TestMethod]
+        public void AndOperationTest4()
+        {
+            CollectionAssert.AreEqual(new byte[] { 0, 0, 1, 0, 1, 0 }, AndOperation(new byte[] { 1, 1, 1, 1, 1, 0 }, new byte[] { 1, 0, 1, 0 }));
+        }
+
+        [TestMethod]
+        public void AndOperationTest5()
+        {
+            CollectionAssert.AreEqual(new byte[] { 0, 0, 1, 0, 0 }, AndOperation(new byte[] { 1, 1, 1, 1, 0 }, new byte[] { 1, 0, 1 }));
+        }
+
+        byte[] ConvertFromBase10ToBase2(int number)
+        {
+            byte[] array = { };
+
+            while (number > 0)
+            {
+                Array.Resize(ref array, array.Length + 1);
+                array[array.Length - 1] = (byte)(number % 2);
+                number /= 2;
+            }
+
+            return Inverter(array);
+        }
+
+
+        private static byte[] Inverter(byte[] array)
+        {
+            byte[] reversedArray = new byte[array.Length];
+            for (int i = 0; i < array.Length; i++) reversedArray[i] = array[array.Length - 1 - i];
+            return reversedArray;
+        }
+
         byte[] MakeArraysToHaveSameLength(byte[] shortArray, byte[] longArray)
         {
             byte[] intermediateArray = new byte[] { };
             intermediateArray = Inverter(shortArray);
-            for ( int i = shortArray.Length; i < longArray.Length; i++)
+            for (int i = shortArray.Length; i < longArray.Length; i++)
             {
                 Array.Resize(ref intermediateArray, intermediateArray.Length + 1);
                 intermediateArray[i] = (byte)0;
             }
-                
             return Inverter(intermediateArray);
 
         }
+
+        byte[] NotOperation(byte[] array)
+        {
+            byte[] notArray = new byte[array.Length];
+            for(int i = 0; i < notArray.Length; i++) notArray[i] = array[i] == (byte)0 ? (byte)1 : (byte)0;
+            return notArray;
+        }
+
+        byte[] AndOperation(byte[] arrayA, byte[] arrayB)
+        {
+            int l = Math.Max(arrayA.Length, arrayB.Length);
+            byte[] result = new byte[l];
+
+            if (arrayA.Length < arrayB.Length) arrayA = MakeArraysToHaveSameLength(arrayA, arrayB);
+            else if (arrayA.Length > arrayB.Length) arrayB = MakeArraysToHaveSameLength(arrayB, arrayA);
+
+            for (int i = 0; i < l; i++) result[i] = (arrayA[i] == (byte)1 && arrayB[i] == (byte)1 ? (byte)1 : (byte)0);
+            return result; 
+        }
+
     }
 }
