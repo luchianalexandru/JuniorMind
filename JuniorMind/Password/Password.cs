b8fc06490ca81cb3703f36b05eb9f687dcf7bc5a
luchianalexandru <luchianalex@yahoo.com>
2016-01-18 20:12:15 +0200

    Password: added the code for a random password, with no limits other than length.

--- JuniorMind/Password/Password.cs
+++ JuniorMind/Password/Password.cs
@@ -4,11 +4,33 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Password
 {
     [TestClass]
-    public class UnitTest1
+    public class Password
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldReturnAPassOfGivenLength()
         {
+            var options = new PasswordOptions { length = 8 };
+            string testPass = GeneratePass(options);
+            Assert.AreEqual(8, testPass.Length);
+        }
+
+        string GeneratePass(PasswordOptions Options)
+        {
+            string pass = "";
+            for (int i = 0; i < Options.length; i++)
+            {
+                pass += (char)random.Next();
+            }
+            return pass;
+        }
+
+        Random random = new Random();
+        public struct PasswordOptions
+        {
+            public int length;
+            public int UpperCase;
+            public int numbers;
+            public int symbols;
         }
     }
 }
