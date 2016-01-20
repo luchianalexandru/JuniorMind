e9e8e6ddeeb649221506a6b16b2614a02a4cb23c
luchianalexandru <luchianalex@yahoo.com>
2016-01-20 22:30:36 +0200

    RoadIntersection: added the structure.

--- JuniorMind/RoadIntersection/RoadIntersection.cs
+++ JuniorMind/RoadIntersection/RoadIntersection.cs
@@ -11,6 +11,18 @@ namespace RoadIntersection
         {
         }
 
+        struct Point
+        {
+            public int x;
+            public int y;
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+
 
     }
 }
