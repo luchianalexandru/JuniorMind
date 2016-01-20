b56cabbeb78a7b571b5d5687324939eda8361970
luchianalexandru <luchianalex@yahoo.com>
2016-01-20 21:49:46 +0200

    RoadIntersection: added new empty project.

---
+++ JuniorMind/RoadIntersection/RoadIntersection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoadIntersection
+{
+    [TestClass]
+    public class RoadIntersection
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+
+    }
+}
