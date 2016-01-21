1b76fe88341b69b7d1a406bb28220f52440143ba
luchianalexandru <luchianalex@yahoo.com>
2016-01-21 21:21:04 +0200

    Football: added the last match of the leg and tested if it works.

--- JuniorMind/Football/Football.cs
+++ JuniorMind/Football/Football.cs
@@ -6,10 +6,7 @@ namespace Football
     [TestClass]
     public class Football
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+  
 
         public struct Match
         {
@@ -19,7 +16,7 @@ namespace Football
             public string losingTeam;
             public int goalsGivenLosingTeam;
             public int goalsReceivedLosingTeam;
-            
+
         }
 
         public Match[] Leg =
@@ -28,5 +25,25 @@ namespace Football
             new Match { winningTeam = "petrolul" , goalsGivenWinningTeam = 2, goalsReceivedWinningTeam = 1, losingTeam = "gloria", goalsGivenLosingTeam = 1, goalsReceivedLosingTeam = 2 },
             new Match { winningTeam = "avantul" , goalsGivenWinningTeam = 1, goalsReceivedWinningTeam = 0, losingTeam = "petrolul", goalsGivenLosingTeam = 0, goalsReceivedLosingTeam = 1 }
         };
+
+        [TestMethod]
+        public void TestsIfTheLastMatchWasAdded()
+        {
+            AddTheLastMatchOfTheLeg("petrolul", "gloria", 7, 2);
+            Assert.AreEqual("petrolul", Leg[3].winningTeam);
+            Assert.AreEqual(7, Leg[3].goalsGivenWinningTeam);
+        }
+
+
+        public void AddTheLastMatchOfTheLeg(string name1, string name2, int goalsGiven, int goalsReceived)
+        {
+            Array.Resize(ref Leg, Leg.Length + 1);
+            Leg[Leg.Length - 1].winningTeam = name1;
+            Leg[Leg.Length - 1].goalsGivenWinningTeam = goalsGiven;
+            Leg[Leg.Length - 1].goalsReceivedWinningTeam = goalsReceived;
+            Leg[Leg.Length - 1].losingTeam = name2;
+            Leg[Leg.Length - 1].goalsGivenLosingTeam = goalsReceived;
+            Leg[Leg.Length - 1].goalsReceivedLosingTeam = goalsGiven;
+        }
     }
 }
