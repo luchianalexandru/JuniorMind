82a983bcbacc4104aacad22b6f02eca73e663918
luchianalexandru <luchianalex@yahoo.com>
2016-01-21 21:02:31 +0200

    Football: added struct for Football.

--- JuniorMind/Football/Football.cs
+++ JuniorMind/Football/Football.cs
@@ -11,6 +11,22 @@ namespace Football
         {
         }
 
+        public struct Match
+        {
+            public string winningTeam;
+            public int goalsGivenWinningTeam;
+            public int goalsReceivedWinningTeam;
+            public string losingTeam;
+            public int goalsGivenLosingTeam;
+            public int goalsReceivedLosingTeam;
+            
+        }
 
+        public Match[] Leg =
+        {
+            new Match { winningTeam = "avantul" , goalsGivenWinningTeam = 1, goalsReceivedWinningTeam = 0, losingTeam = "gloria", goalsGivenLosingTeam = 0, goalsReceivedLosingTeam = 1 },
+            new Match { winningTeam = "petrolul" , goalsGivenWinningTeam = 2, goalsReceivedWinningTeam = 1, losingTeam = "gloria", goalsGivenLosingTeam = 1, goalsReceivedLosingTeam = 2 },
+            new Match { winningTeam = "avantul" , goalsGivenWinningTeam = 1, goalsReceivedWinningTeam = 0, losingTeam = "petrolul", goalsGivenLosingTeam = 0, goalsReceivedLosingTeam = 1 }
+        };
     }
 }
