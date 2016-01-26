620f8fbf7fb55c2306cb77bc6045d0744735efa1
luchianalexandru <luchianalex@yahoo.com>
2016-01-26 23:50:16 +0200

    Fibonacci: added solution and a few more tests to make sure it works.

--- JuniorMind/Fibonacci/Fibonacci.cs
+++ JuniorMind/Fibonacci/Fibonacci.cs
@@ -6,16 +6,35 @@ namespace Fibonacci
     [TestClass]
     public class Fibonacci
     {
+
+        [TestMethod]
+        public void TestFibonacciFor0()
+        {
+            Assert.AreEqual(0, FindFibonacciForGivenNumber(0));
+        }
+
+        [TestMethod]
+        public void TestFibonacciFor1()
+        {
+            Assert.AreEqual(1, FindFibonacciForGivenNumber(1));
+        }
+
+        [TestMethod]
+        public void TestFibonacciFor2()
+        {
+            Assert.AreEqual(1, FindFibonacciForGivenNumber(2));
+        }
+
         [TestMethod]
         public void TestFibonacciFor3()
         {
-            Assert.AreEqual(1, FindFibonacciForGivenNumber(3));
+            Assert.AreEqual(2, FindFibonacciForGivenNumber(3));
         }
 
         public int FindFibonacciForGivenNumber(int n)
         {
-
-            return 0;
+            if ((n == 0) || (n == 1)) return n;
+            return FindFibonacciForGivenNumber(n - 1) + FindFibonacciForGivenNumber(n - 2);  
         }
     }
 }
