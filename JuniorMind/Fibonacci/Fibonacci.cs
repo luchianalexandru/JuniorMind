0a488d99191a00972154746d4aa535734c57e445
luchianalexandru <luchianalex@yahoo.com>
2016-01-26 23:35:44 +0200

    Fibonacci: added empty project.

---
+++ JuniorMind/Fibonacci/Fibonacci.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fibonacci
+{
+    [TestClass]
+    public class Fibonacci
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
