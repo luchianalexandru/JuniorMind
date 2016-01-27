45766607b5321ff908e71c52f95133365854ed81
luchianalexandru <luchianalex@yahoo.com>
2016-01-27 19:30:55 +0200

    StringInverterRecursive: added empty project.

---
+++ JuniorMind/StringInverterRecursive/StringInverterRecursive.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringInverterRecursive
+{
+    [TestClass]
+    public class StringInverterRecursive
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
