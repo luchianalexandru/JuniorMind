0e94adf9f1efdad68e58da23bcda19576876179c
luchianalexandru <luchianalex@yahoo.com>
2016-01-28 20:47:29 +0200

    ReplaceCharWithStringRecursive: added the solution for the rest of the string and some tests.

--- JuniorMind/ReplaceCharWithStringRecursive/ReplaceCharWithStringRecursive.cs
+++ JuniorMind/ReplaceCharWithStringRecursive/ReplaceCharWithStringRecursive.cs
@@ -6,24 +6,38 @@ namespace ReplaceCharWithStringRecursive
     [TestClass]
     public class ReplaceCharWithStringRecursive
     {
-
         [TestMethod]
         public void ReplaceCharWithStringTest1()
         {
-            Assert.AreEqual("bc", CharReplaceWithString("a", 'a', "bc"));
+            Assert.AreEqual("", CharReplaceWithString("", 'a', "bc"));
         }
 
         [TestMethod]
         public void ReplaceCharWithStringTest2()
         {
-            Assert.AreEqual("", CharReplaceWithString("", 'a', "bc"));
+            Assert.AreEqual("bc", CharReplaceWithString("a", 'a', "bc"));
+        }
+
+        [TestMethod]
+        public void ReplaceCharWithStringTest3()
+        {
+            Assert.AreEqual("abefd", CharReplaceWithString("abcd", 'c', "ef"));
+        }
+
+        [TestMethod]
+        public void ReplaceCharWithStringTest4()
+        {
+            Assert.AreEqual("chechecheche", CharReplaceWithString("chchchch", 'h', "he"));
         }
 
 
-        public static string CharReplaceWithString(string s, char a, string b)
+        public static string CharReplaceWithString(string s, char toReplace, string replacement)
         {
             if (s.Length < 1) return "";
-            return s; 
+            string result = "";
+            if (s[0] == toReplace) result = replacement;
+            else result = s[0].ToString(); 
+            return result + CharReplaceWithString(s.Substring(1, s.Length-1), toReplace, replacement); 
 
         }
     }
