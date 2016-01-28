0b482b02c7b212eb4330159ae6cb7989bc07c09d
luchianalexandru <luchianalex@yahoo.com>
2016-01-28 20:38:14 +0200

    ReplaceCharWithStringRecursive: made first test pass.

--- JuniorMind/ReplaceCharWithStringRecursive/ReplaceCharWithStringRecursive.cs
+++ JuniorMind/ReplaceCharWithStringRecursive/ReplaceCharWithStringRecursive.cs
@@ -6,15 +6,23 @@ namespace ReplaceCharWithStringRecursive
     [TestClass]
     public class ReplaceCharWithStringRecursive
     {
+
         [TestMethod]
         public void ReplaceCharWithStringTest1()
         {
             Assert.AreEqual("bc", CharReplaceWithString("a", 'a', "bc"));
         }
 
+        [TestMethod]
+        public void ReplaceCharWithStringTest2()
+        {
+            Assert.AreEqual("", CharReplaceWithString("", 'a', "bc"));
+        }
+
 
         public static string CharReplaceWithString(string s, char a, string b)
         {
+            if (s.Length < 1) return "";
             return s; 
 
         }
