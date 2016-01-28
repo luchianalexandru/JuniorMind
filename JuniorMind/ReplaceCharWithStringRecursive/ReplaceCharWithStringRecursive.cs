9623e4508775e4d9c754568d54be723849afdd6a
luchianalexandru <luchianalex@yahoo.com>
2016-01-28 20:30:10 +0200

    ReplaceCharWithStringRecursive: added empty project

---
+++ JuniorMind/ReplaceCharWithStringRecursive/ReplaceCharWithStringRecursive.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReplaceCharWithStringRecursive
+{
+    [TestClass]
+    public class ReplaceCharWithStringRecursive
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
