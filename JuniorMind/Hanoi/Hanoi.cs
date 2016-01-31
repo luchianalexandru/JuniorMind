8a1ca3bb6c7f63a753439d950409599b99286ea3
luchianalexandru <luchianalex@yahoo.com>
2016-01-31 12:15:18 +0200

    Hanoi: added empty project

---
+++ JuniorMind/Hanoi/Hanoi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hanoi
+{
+    [TestClass]
+    public class Hanoi
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+
+    }
+}
