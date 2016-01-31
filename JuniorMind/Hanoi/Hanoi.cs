669b5879cf99f3a6e27ae2cf22109635b42c88eb
luchianalexandru <luchianalex@yahoo.com>
2016-01-31 15:54:01 +0200

    Hanoi: added the solution and some tests.

--- JuniorMind/Hanoi/Hanoi.cs
+++ JuniorMind/Hanoi/Hanoi.cs
@@ -7,10 +7,35 @@ namespace Hanoi
     public class Hanoi
     {
         [TestMethod]
-        public void TestMethod1()
+        public void HanoiTestFor1()
         {
+            Assert.AreEqual("AC", Solution(1, 'A', 'B', 'C'));
         }
 
+        [TestMethod]
+        public void HanoiTestFor2()
+        {
+            Assert.AreEqual("AB AC BC", Solution(2, 'A', 'B', 'C'));
+        }
 
+        [TestMethod]
+        public void HanoiTestFor3()
+        {
+            Assert.AreEqual("AC AB CB AC BA BC AC", Solution(3, 'A', 'B', 'C'));
+        }
+
+        public string Solution(int n, char source, char aux, char destination )
+        {
+            string result = string.Empty;
+            if (n == 1)
+            {
+                return $"{source}{destination}";
+            }
+            result += Solution(n - 1, source, destination, aux);
+            result += " " + Solution(1, source, aux, destination);
+            result += " " + Solution(n - 1, aux, source, destination);
+            return result;
+
+        }
     }
 }
