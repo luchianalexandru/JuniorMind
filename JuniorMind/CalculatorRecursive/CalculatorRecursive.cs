561e903c51a65e1ab3722f513fa33af7df08cd5d
luchianalexandru <luchianalex@yahoo.com>
2016-02-01 21:14:09 +0200

    CalculatorRecursive: added more tests for the rest of the operations.

--- JuniorMind/CalculatorRecursive/CalculatorRecursive.cs
+++ JuniorMind/CalculatorRecursive/CalculatorRecursive.cs
@@ -7,23 +7,52 @@ namespace CalculatorRecursive
     public class CalculatorRecursive
     {
         [TestMethod]
-        public void CalculatorFirstTestSumm()
+        public void CalculatorTestSumm()
         {
             Assert.AreEqual(5, Calculate("+ 2 3"));
         }
 
         [TestMethod]
-        public void CalculatorSecondTestDoubleNumbers()
+        public void CalculatorTestDoubleNumbers()
         {
             Assert.AreEqual(-1, Calculate("+ 3 -4"));
         }
 
         [TestMethod]
-        public void CalculatorSecondTestMoreOperations()
-        {
+        public void CalculatorTestMoreThanOneOperation()
+        { 
             Assert.AreEqual(3, Calculate("+ + + 3 -4 7 -3"));
         }
 
+        [TestMethod]
+        public void CalculatorTestSubstraction()
+        {
+            Assert.AreEqual(0, Calculate("- -1 -1"));
+        }
+
+        [TestMethod]
+        public void CalculatorAnotherTestSubstraction()
+        {
+            Assert.AreEqual(7, Calculate("- 6 -1"));
+        }
+
+        [TestMethod]
+        public void CalculatorTestSubstractionAndSum()
+        {
+            Assert.AreEqual(-11, Calculate("+ - + 3 -4 7 -3"));
+        }
+
+        [TestMethod]
+        public void CalculatorTestAdding()
+        {
+            Assert.AreEqual(112.5, Calculate("* * 3 5 7.5"));
+        }
+
+        [TestMethod]
+        public void CalculatorTestDivision()
+        {
+            Assert.AreEqual(15, Calculate("/ 112.5 7.5"));
+        }
 
         public double Calculate(string operation)
         {
@@ -51,6 +80,12 @@ namespace CalculatorRecursive
             {
                 case "+":
                     return Calculate(array, ref pos) + Calculate(array, ref pos);
+                case "-":
+                    return Calculate(array, ref pos) - Calculate(array, ref pos);
+                case "*":
+                    return Calculate(array, ref pos) * Calculate(array, ref pos);
+                case "/":
+                    return Calculate(array, ref pos) / Calculate(array, ref pos);
                 default:
                     return 0;
             }
