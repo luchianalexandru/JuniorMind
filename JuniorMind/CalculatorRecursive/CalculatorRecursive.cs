e8ee136e727876efda8729e80c832f074cf8bce9
luchianalexandru <luchianalex@yahoo.com>
2016-02-01 20:32:50 +0200

    CalculatorRecursive: added the function that checks if operation or number and that increments if number.

--- JuniorMind/CalculatorRecursive/CalculatorRecursive.cs
+++ JuniorMind/CalculatorRecursive/CalculatorRecursive.cs
@@ -7,17 +7,34 @@ namespace CalculatorRecursive
     public class CalculatorRecursive
     {
         [TestMethod]
-        public void CalculatorFirstTest()
+        public void CalculatorFirstTestSumm()
         {
-            Assert.AreEqual(0, Calculate("+ 2 3"));
+            Assert.AreEqual(5, Calculate("+ 2 3"));
+        }
+
+        [TestMethod]
+        public void CalculatorSecondTestDoubleNumbers()
+        {
+            Assert.AreEqual(-1, Calculate("+ 3 -4"));
         }
 
         public double Calculate(string operation)
         {
+            int pos = 0;
             string[] array = operation.Split(' ');
-            return 0;
+            return Calculate(array, ref pos);
         }
 
-
+        public double Calculate(string[] operation, ref int pos)
+        {
+            double result = 0;
+            if (double.TryParse(operation[ref pos], out result))
+            {
+                pos++;
+                return result;
+            }
+            pos++;
+            return Calculate(operation, ref pos) + Calculate(operation, ref pos);
+        }
     }
 }
