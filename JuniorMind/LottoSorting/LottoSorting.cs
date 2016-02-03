8d8ed0165c804dec6e5f98804f5c4a89acbd12e5
luchianalexandru <luchianalex@yahoo.com>
2016-02-03 19:19:56 +0200

    LottoSorting: added new empty project.

---
+++ JuniorMind/LottoSorting/LottoSorting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LottoSorting
+{
+    [TestClass]
+    public class LottoSorting
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
