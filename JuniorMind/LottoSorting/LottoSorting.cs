396faf63fd45084f725cf5af23e3b14f5647c21c
luchianalexandru <luchianalex@yahoo.com>
2016-02-03 19:29:36 +0200

    LottoSorting: added the insert algorithm solution and tests

--- JuniorMind/LottoSorting/LottoSorting.cs
+++ JuniorMind/LottoSorting/LottoSorting.cs
@@ -7,9 +7,41 @@ namespace LottoSorting
     public class LottoSorting
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SortTest1()
         {
+            int[] numbers = new int[] { 3, 1, 2, 5, 4 };
+            SortArray(numbers);
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5 }, numbers);
         }
 
+
+        [TestMethod]
+        public void SortTest2()
+        {
+            int[] numbers = new int[10] { 3, 1, 2, 5, 4, 7, 10, 9, 6, 8 };
+            SortArray(numbers);
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, numbers);
+        }
+
+
+        public void SortArray(int[] numArray)
+        {
+            int j;
+            for(int i = 0; i < numArray.Length; i++)
+            {
+                j = i;
+                while(j > 0)
+                {
+                    if (numArray[j - 1] > numArray[j])
+                    {
+                        int temp = numArray[j - 1];
+                        numArray[j - 1] = numArray[j];
+                        numArray[j] = temp;
+                        j--;
+                    }
+                    else break;
+                }
+            }
+        }
     }
 }
