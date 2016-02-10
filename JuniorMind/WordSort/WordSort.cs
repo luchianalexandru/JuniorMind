d3bdab6422db86c39df3d2bd92aa10f98f8bddec
luchianalexandru <luchianalex@yahoo.com>
2016-02-10 23:39:26 +0200

    WordSort: rethought the repetition count using a temp array and resized the array for each new element found.

--- JuniorMind/WordSort/WordSort.cs
+++ JuniorMind/WordSort/WordSort.cs
@@ -7,9 +7,9 @@ namespace WordSort
     public class WordSort
     {
         [TestMethod]
-        public void Test1()
+        public void TestsTheStructCountForTwoElements()
         {
-            var structArray = new WordCount[] { new WordCount("abc", 2), new WordCount("bca", 1), new WordCount("abc", 2) };
+            var structArray = new WordCount[] { new WordCount("abc", 2), new WordCount("bca", 1) };
             CollectionAssert.AreEqual(structArray, SortByTextNrOfRepetition("abc bca abc"));
         }
 
@@ -17,22 +17,30 @@ namespace WordSort
         public WordCount[] SortByTextNrOfRepetition(string textToSort)
         {
             string[] array = textToSort.Split(' ');
-            var wordCount = new WordCount[array.Length];
+            WordCount[] wordCount = new WordCount[0];
+            string[] tempArray = new string[0];
 
-            for (int i = 0; i < array.Length; i++)
+            while (array.Length > 0)
             {
-                int count = 0;
-                for (int j = 0; j < array.Length; j++)
+                int count = 1;
+                for (int j = 1; j < array.Length; j++)
                 {
+                    if (array[0] == array[j]) count++;
+                    else
+                    {
+                        Array.Resize(ref tempArray, tempArray.Length + 1);
+                        tempArray[tempArray.Length - 1] = array[j];
+                    }
 
-                    if (array[i] == array[j])
-                        count++;
                 }
-                wordCount[i].word = array[i];
-                wordCount[i].repetitions = count;
+                Array.Resize(ref wordCount, wordCount.Length + 1);
+                wordCount[wordCount.Length - 1] = new WordCount(array[0], count);
+                array = tempArray;
+                tempArray = new string[0];
             }
-                return wordCount;
+            return wordCount;
         }
+
 
         public struct WordCount
         {
