f8317e8a787b8657f02e4930c4b422e6374edcd1
luchianalexandru <luchianalex@yahoo.com>
2016-02-10 22:11:22 +0200

    WordSort: attempt at counting the repetitions

--- JuniorMind/WordSort/WordSort.cs
+++ JuniorMind/WordSort/WordSort.cs
@@ -7,8 +7,10 @@ namespace WordSort
     public class WordSort
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test1()
         {
+            var structArray = new WordCount[] { new WordCount("abc", 2), new WordCount("bca", 1)};
+            CollectionAssert.AreEqual(structArray, SortByTextNrOfRepetition("abc abc bca"));
         }
 
 
@@ -17,7 +19,19 @@ namespace WordSort
             string[] array = textToSort.Split(' ');
             var wordCount = new WordCount[array.Length];
 
-            return wordCount;
+            for (int i = 0; i < array.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+
+                    if (array[i] == array[j])
+                        count++;
+                }
+                wordCount[i].word = array[i];
+                wordCount[i].repetitions = count;
+            }
+                return wordCount;
         }
 
         public struct WordCount
