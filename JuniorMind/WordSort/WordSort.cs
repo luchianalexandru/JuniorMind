bd010934c389757b9921e763acffa774d58b4179
luchianalexandru <luchianalex@yahoo.com>
2016-02-10 23:46:35 +0200

    WordSort: added the sort and swap functions and fixed the initial tests to show the sorted structure.

--- JuniorMind/WordSort/WordSort.cs
+++ JuniorMind/WordSort/WordSort.cs
@@ -23,8 +23,8 @@ namespace WordSort
         [TestMethod]
         public void TestsTheStructCountForThreeElements()
         {
-            var structArray = new WordCount[] { new WordCount("bca", 2), new WordCount("abc", 4), new WordCount("cde", 1) };
-            CollectionAssert.AreEqual(structArray, SortByTextNrOfRepetition("bca abc abc cde bca abc abc"));
+            var structArray = new WordCount[] { new WordCount("abc", 4), new WordCount("bca", 2), new WordCount("cde", 1) };
+            CollectionAssert.AreEqual(structArray, SortByTextNrOfRepetition("cde bca abc abc bca abc abc"));
         }
 
         public WordCount[] SortByTextNrOfRepetition(string textToSort)
@@ -51,7 +51,39 @@ namespace WordSort
                 array = tempArray;
                 tempArray = new string[0];
             }
+            Sort(ref wordCount);
             return wordCount;
+        }
+
+        public void Sort(ref WordCount[] wordCount)
+        {
+            for (int i = 0; i < wordCount.Length; i++)
+            {
+                for (int j = wordCount.Length - 1; j > i; j--)
+                {
+                    if (wordCount[i].repetitions < wordCount[j].repetitions)
+                    {
+                        Swap(ref wordCount[i], ref wordCount[j]);
+                    }
+                }
+            }
+        }
+
+
+        [TestMethod]
+        public void SwapTest()
+        {
+            var structArray = new WordCount[] { new WordCount("abc", 2), new WordCount("bca", 1) };
+            var expectedArray = new WordCount[] { new WordCount("bca", 1), new WordCount("abc", 2) };
+            Swap(ref structArray[0], ref structArray[1]);
+            CollectionAssert.AreEqual(expectedArray, structArray);
+        }
+
+        private void Swap(ref WordCount first, ref WordCount second)
+        {
+            WordCount temp = first;
+            first = second;
+            second = temp;
         }
 
 
