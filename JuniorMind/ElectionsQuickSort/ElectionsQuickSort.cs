943a5a1a429210154fc003e915747eed72817beb
luchianalexandru <luchianalex@yahoo.com>
2016-02-15 20:39:14 +0200

    ElectionsQuickSort: added the functions that centralize the votes and get the total of votes for each candidate.

--- JuniorMind/ElectionsQuickSort/ElectionsQuickSort.cs
+++ JuniorMind/ElectionsQuickSort/ElectionsQuickSort.cs
@@ -15,6 +15,64 @@ namespace ElectionsQuickSort
             CollectionAssert.AreEqual(expected, office);
         }
 
+        [TestMethod]
+        public void TestsIfTheVotesAreCentralizedForTwoOfficesAndTwoPoliticians()
+        {
+            Politician[] office1 = { new Politician("zagrean", 10), new Politician("tamas", 5) };
+            Politician[] office2 = { new Politician("zagrean", 5), new Politician("tamas", 6) };
+            Politician[] office3 = { new Politician("zagrean", 20), new Politician("tamas", 3) };
+            VotesPerOffice[] allResults =
+            {
+               new VotesPerOffice(office1),
+               new VotesPerOffice(office2),
+               new VotesPerOffice(office3)
+            };
+
+            Politician[] expected = { new Politician("zagrean", 35), new Politician("tamas", 14) };
+            CollectionAssert.AreEqual(expected, GetTotalOfVotes(allResults));
+        }
+
+        [TestMethod]
+        public void TestsIfTheVotesAreCentralizedForThreeOfficesAndThreePoliticians()
+        {
+            Politician[] office1 = { new Politician("zagrean", 10), new Politician("tamas", 5), new Politician("baciu", 7) };
+            Politician[] office2 = { new Politician("zagrean", 5), new Politician("tamas", 6), new Politician("baciu", 9) };
+            Politician[] office3 = { new Politician("zagrean", 20), new Politician("tamas", 3), new Politician("baciu", 12) };
+            VotesPerOffice[] allResults =
+            {
+               new VotesPerOffice(office1),
+               new VotesPerOffice(office2),
+               new VotesPerOffice(office3)
+            };
+
+            Politician[] expected = { new Politician("zagrean", 35), new Politician("tamas", 14), new Politician("baciu", 28) };
+            CollectionAssert.AreEqual(expected, GetTotalOfVotes(allResults));
+        }
+
+        private static Politician[] CentralizeVotes(VotesPerOffice[] results)
+        {
+            Politician[] total = new Politician[results[0].office.Length];
+            for (int i = 0; i < total.Length; i++)
+            {
+                total[i].name = results[0].office[i].name;
+                total[i].votes = 0;
+            }
+            return total;
+        }
+
+        public Politician[] GetTotalOfVotes(VotesPerOffice[] results)
+        {
+            Politician[] totalVotes = CentralizeVotes(results);
+            for (int i = 0; i < results.Length; i++)
+            {
+                for (int j = 0; j < totalVotes.Length; j++) 
+                {
+                    totalVotes[j].votes += results[i].office[j].votes;
+                }
+            }
+            return totalVotes;
+        }
+
 
         static void Swap(ref Politician a, ref Politician b)
         {
@@ -31,6 +89,7 @@ namespace ElectionsQuickSort
                 this.office = office;
             }
         }
+
         public struct Politician
         {
             public string name;
