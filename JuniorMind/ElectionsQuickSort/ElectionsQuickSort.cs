913d4565b3edbac425f8669cb32c22278fe653e5
luchianalexandru <luchianalex@yahoo.com>
2016-02-15 19:30:04 +0200

    ElectionsQuickSort: added the structures.

--- JuniorMind/ElectionsQuickSort/ElectionsQuickSort.cs
+++ JuniorMind/ElectionsQuickSort/ElectionsQuickSort.cs
@@ -10,5 +10,24 @@ namespace ElectionsQuickSort
         public void TestMethod1()
         {
         }
+
+        public struct VotesPerOffice
+        {
+            public Politician[] office;
+            public VotesPerOffice(Politician[] office)
+            {
+                this.office = office;
+            }
+        }
+        public struct Politician
+        {
+            public string name;
+            public int votes;
+            public Politician(string name, int votes)
+            {
+                this.name = name;
+                this.votes = votes;
+            }
+        }
     }
 }
