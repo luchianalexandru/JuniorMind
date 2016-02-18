91705d455047abd9f7d4205842a506ea9b2743e4
luchianalexandru <luchianalex@yahoo.com>
2016-02-18 19:22:31 +0200

    Catalog: added a new empty project.

---
+++ JuniorMind/Catalog/Catalog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Catalog
+{
+    [TestClass]
+    public class Catalog
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
