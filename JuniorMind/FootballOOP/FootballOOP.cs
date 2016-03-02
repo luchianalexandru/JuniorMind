fb40fa88ce31996cbcc27021b63418d08ee3ea24
luchianalexandru <luchianalex@yahoo.com>
2016-03-02 21:42:17 +0200

    FootballOOP: added new empty project with OOP

---
+++ JuniorMind/FootballOOP/FootballOOP.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FootballOOP
+{
+    [TestClass]
+    public class FootballOOP
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
