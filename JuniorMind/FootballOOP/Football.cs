b0797725be74f49e789329a468825b573d151997
luchianalexandru <luchianalex@yahoo.com>
2016-03-02 22:09:27 +0200

    FootballOOP: added some sort of class.

--- JuniorMind/FootballOOP/Football.cs
+++ JuniorMind/FootballOOP/Football.cs
@@ -8,5 +8,26 @@ namespace FootballOOP
 {
     class Football
     {
+        public class Match
+        {
+            private string winningTeam;
+            private string losingTeam;
+            private int winningGoals;
+            private int losingGoals;
+            public Match(string winningTeam, string losingTeam, int winningGoals, int losingGoals)
+            {
+                this.winningTeam = winningTeam;
+                this.losingTeam = losingTeam;
+                this.winningGoals = winningGoals;
+                this.losingGoals = losingGoals;
+            }
+        }
+
+        public class Leg
+        {
+            Match newMatch1 = new Match("avantul", "gloria", 1, 1);
+            Match newMatch2 = new Match("sageata", "concordia", 7, 1);
+            Match newMatch3 = new Match("astra", "ceahlaul", 3, 1);
+        };
     }
 }
