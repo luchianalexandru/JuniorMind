8faaaedde1f1224fef4f7b38107ccb1c907deb92
luchianalexandru <luchianalex@yahoo.com>
2016-03-20 10:18:19 +0200

    Vector: added a new empty project.

---
+++ JuniorMind/Vector/VectorTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vector;
+
+namespace Vector
+{
+    [TestClass]
+    public class VectorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
