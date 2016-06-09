8e9f23b7ecf1ed9c788b8c6c8859160fa13af2bf
luchianalexandru <luchianalex@yahoo.com>
2016-06-09 21:46:59 +0300

    WebAppPassword: works rudimentary.

--- JuniorMind/JuniorMind/Library/PasswordOptions.cs
+++ JuniorMind/JuniorMind/Library/PasswordOptions.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    class Class1
+    public struct PasswordOptions
     {
+        public int Length { get; set; }
+        public int UpperCase { get; set; }
+        public int Numbers { get; set; }
+        public int Symbols { get; set; }
+        public bool Ambiguous { get; set; }
+        public bool Similar { get; set; }
     }
 }
+
