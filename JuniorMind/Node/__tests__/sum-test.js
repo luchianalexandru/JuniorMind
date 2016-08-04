62c1485063de58f25ab1a6f2ae71ec1ca1e7a67a
luchianalexandru <luchianalex@yahoo.com>
2016-08-04 20:29:33 +0300

    Node.js added Babel integration and ran tests.

--- JuniorMind/Node/__tests__/sum-test.js
+++ JuniorMind/Node/__tests__/sum-test.js
@@ -2,9 +2,18 @@
 
 jest.unmock('../sum');
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
 describe('sum', () => {
   it('adds 1 + 2 to equal 3', () => {
     const sum = require('../sum');
     expect(sum(1, 2)).toBe(3);
+  });
+
+  it('adds 1 + 2 not equal 4', () => {
+  const sum = require('../sum');
+  expect(sum(1, 2)).not.toBe(4);
   });
 });
